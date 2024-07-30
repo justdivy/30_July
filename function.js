93bd0fa83309f911501cf321c95c5b51dc62768c
Divyansh Verma <vdivy95@gmail.com>
2024-07-30 10:11:48 +0530

    Initial commit

---
+++ function.js
@@ -0,0 +1,12 @@
+const person = {
+    firstName :"Raj",
+    LastName : "Soni",
+    fullName : function(){
+        return this.firstName + " " + this.LastName;
+    },
+};
+
+const person1 = {
+    firstName: "John",
+    LastName: "Doe"
+}
